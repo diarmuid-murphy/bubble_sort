848f14e33ac61c549e8ed52dcb531fe7d69e8ff5
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-10-22 00:28:09 -0500

    one successful swap

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,11 +3,16 @@ def bubble_sort(numbers)
 	swapped = false
 
 	while swapped == false do
-		for i in 0..n-1
-
+		for i in 1..n-1
+			if numbers[i-1] > numbers[i]
+				numbers[i-1],numbers[i] = numbers[i], numbers[i-1]
+				puts numbers[0]
+				swapped = true # leave this in for testing until propetly implemented.
+			end
 		end
-		swapped = true # leave this in for testing until propetly implemented.
 	end
+
+	puts numbers.inspect
 end
 
 bubble_sort([4,3,78,2,0,2]) # should return [0,2,2,3,4,78]
