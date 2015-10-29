f0bb6440e8c08eadc6390c8c02e82ccb5c8648da
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-10-28 22:15:21 -0500

    complete :)

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,15 @@
 def bubble_sort(numbers)
 	n = numbers.length
-	swapped = false
+	swapped = true
 
-	while swapped == false do
+	(n-1).times do
 		for i in 1..n-1
 			if numbers[i-1] > numbers[i]
 				numbers[i-1],numbers[i] = numbers[i], numbers[i-1]
-				puts numbers[0]
-				swapped = true # leave this in for testing until propetly implemented.
 			end
 		end
+		puts numbers.inspect
 	end
-
-	puts numbers.inspect
 end
 
 bubble_sort([4,3,78,2,0,2]) # should return [0,2,2,3,4,78]
